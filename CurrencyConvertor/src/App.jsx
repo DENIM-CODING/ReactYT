1ac032d927142607f282f2d760a330b50aef4e79
DENIM <denim@DENIMs-MacBook-Air.local>
2025-05-13 15:40:15 +0530

    Initial Commit

---
+++ CurrencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-amber-900'>Currency Convertor</h1>
+    </>
+  )
+}
+
+export default App
